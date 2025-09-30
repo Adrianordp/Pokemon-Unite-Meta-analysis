89f78bd46d408a742e2e6480c314605130b48bd2
Adriano Rodrigues <adrianordp1@gmail.com>
2025-09-29 21:50:23 -0400

    refactor[src/pokemon_unite_meta_analysis/sort_by.py]: change SortBy to inherit from str

--- src/pokemon_unite_meta_analysis/sort_by.py
+++ src/pokemon_unite_meta_analysis/sort_by.py
@@ -5,7 +5,7 @@ Defines sorting criteria for builds based on different attributes.
 from enum import Enum
 
 
-class SortBy(Enum):
+class SortBy(str, Enum):
     """
     SortBy enum class
     """
@@ -14,8 +14,6 @@ class SortBy(Enum):
     ROLE = "role"
     POKEMON_WIN_RATE = "pokemon_win_rate"
     POKEMON_PICK_RATE = "pokemon_pick_rate"
-    MOVE_1 = "move_1"
-    MOVE_2 = "move_2"
     MOVESET_WIN_RATE = "moveset_win_rate"
     MOVESET_PICK_RATE = "moveset_pick_rate"
     MOVESET_TRUE_PICK_RATE = "moveset_true_pick_rate"
