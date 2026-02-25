1e8b2428bb9f5814ef08e9545866be0e0069dee2
Adriano Rodrigues <adrianordp1@gmail.com>
2026-02-25 15:05:04 -0400

    feat[src/dashboard/dashboard.py]: stretch plotly chart width

--- src/dashboard/dashboard.py
+++ src/dashboard/dashboard.py
@@ -381,4 +381,4 @@ if not data.empty:
     if x_axis in ("pokemon", "move 1", "move 2", "item"):
         fig.update_xaxes(tickangle=-90)
 
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, width="stretch")
